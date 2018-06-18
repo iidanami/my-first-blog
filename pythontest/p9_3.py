17c0a989c4e8448523e4f7540e6838657ee80797
iidanami <40352106+iidanami@users.noreply.github.com>
2018-06-18 18:17:52 +0900

    Update p9_3.py

--- pythontest/p9_3.py
+++ pythontest/p9_3.py
@@ -1,20 +1,22 @@
+from __future__ import print_function
 import numpy as np
-import matplotlib
-matplotlib.use('Agg')
-
+import matplotlib.cbook as cbook
+import matplotlib.image as image
 import matplotlib.pyplot as plt
 
 # Fixing random state for reproducibility
 np.random.seed(19680801)
 
 
+datafile = cbook.get_sample_data('logo2.png', asfileobj=False)
+print('loading %s' % datafile)
+im = image.imread(datafile)
+im[:, :, -1] = 0.5  # set the alpha channel
+
 fig, ax = plt.subplots()
+
 ax.plot(np.random.rand(20), '-o', ms=20, lw=2, alpha=0.7, mfc='orange')
 ax.grid()
-
-# position bottom right
-fig.text(0.95, 0.05, 'Property of MPL',
-         fontsize=50, color='gray',
-         ha='right', va='bottom', alpha=0.5)
+fig.figimage(im, 10, 10, zorder=3)
 
 plt.show()
